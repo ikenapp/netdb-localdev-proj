0fe33e505eb9d8c98bfca526326ebc6ac6dc7cf4
adams@netdb.com.tw <adams@netdb.com.tw@d76db217-b173-de39-e541-d259961cc7ae>
2014-04-02 04:00:34 +0000

    Alert Mail 判斷來自資料庫

--- trunk/WoWiV2/Project/CertificateMail.aspx.cs
+++ trunk/WoWiV2/Project/CertificateMail.aspx.cs
@@ -5,88 +5,140 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Data;
+using System.Data.SqlClient;
+
 public partial class Project_CertificateMail : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    //if (Cache["IsMailCertificateRenewalAlert"] == null)
+    if (IsAlertMailExist() == false)
     {
-        if (Cache["IsMailCertificateRenewalAlert"] == null)
-        {
-            if (GridViewProjectTarget.Rows.Count > 0)
-            {
-                string[] mailto = { "ima1@wowiapproval.com", "ima2@wowiapproval.com", "ima3@wowiapproval.com", "ima4@wowiapproval.com", "sales-1@wowiapproval.com", "sales-2@wowiapproval.com", "sales-3@wowiapproval.com", "sales-4@wowiapproval.com", "sales-5@wowiapproval.com", "sales-6@wowiapproval.com" };
-                string[] mailcc = { "Shirley.Kang@WoWiApproval.com", "Scott.Wang@WoWiApproval.com", "Miranda.Chen@WoWiApproval.com" };
-                string[] mailbcc = { "adams@netdb.com.tw" };
-                MailUtil.SendHTMLMail("dbservice@wowiapproval.com", mailto, mailcc, mailbcc, "Certificate Renewal Alert", PanelMail);
-                Cache.Insert("IsMailCertificateRenewalAlert", "1", null
-               , DateTime.Now.AddDays(1)
-               , System.Web.Caching.Cache.NoSlidingExpiration);
-            }
-        }
+      if (GridViewProjectTarget.Rows.Count > 0)
+      {
+        string[] mailto = { "ima1@wowiapproval.com", "ima2@wowiapproval.com", "ima3@wowiapproval.com", "ima4@wowiapproval.com", "sales-1@wowiapproval.com", "sales-2@wowiapproval.com", "sales-3@wowiapproval.com", "sales-4@wowiapproval.com", "sales-5@wowiapproval.com", "sales-6@wowiapproval.com" };
+        string[] mailcc = { "Shirley.Kang@WoWiApproval.com", "Scott.Wang@WoWiApproval.com", "Miranda.Chen@WoWiApproval.com" };
+        //string[] mailto = { "adams@netdb.com.tw" };
+        //string[] mailcc = { "adams@netdb.com.tw" };
+        string[] mailbcc = { "adams@netdb.com.tw" };
+        MailUtil.SendHTMLMail("dbservice@wowiapproval.com", mailto, mailcc, mailbcc, "Certificate Renewal Alert", PanelMail);
+        // Cache.Insert("IsMailCertificateRenewalAlert", "1", null
+        //, DateTime.Now.AddDays(1)
+        //, System.Web.Caching.Cache.NoSlidingExpiration);
+        SetAlertMail();
+      }
+    }
+  }
+
+  protected void GridViewProjectTarget_RowDataBound(object sender, GridViewRowEventArgs e)
+  {
+    if (e.Row.RowType == DataControlRowType.DataRow)
+    {
+      Label lbl_certificate_issue_date = e.Row.FindControl("lbl_certificate_issue_date") as Label;
+      Label lbl_certificate_expiry_date = e.Row.FindControl("lbl_certificate_expiry_date") as Label;
+
+      Label lbl_certificate_duration = e.Row.FindControl("lbl_certificate_duration") as Label;
+      Label lbl_time_until_expiration = e.Row.FindControl("lbl_time_until_expiration") as Label;
+      Label lbl_today_date = e.Row.FindControl("lbl_today_date") as Label;
+      lbl_today_date.Text = DateTime.Now.ToString("yyyy/MM/dd");
+      //計算certificate_duration
+      if (!string.IsNullOrEmpty(lbl_certificate_issue_date.Text) && !string.IsNullOrEmpty(lbl_certificate_expiry_date.Text))
+      {
+        DateTime dt_issue = DateTime.Parse(lbl_certificate_issue_date.Text);
+        DateTime dt_expiry = DateTime.Parse(lbl_certificate_expiry_date.Text);
+        int year = dt_expiry.Year - dt_issue.Year; //相差的年份
+        int month = dt_expiry.Month - dt_issue.Month;  //相差的月份 
+        int day = dt_expiry.Day - dt_issue.Day;//相差的天數 
+        lbl_certificate_duration.Text = year.ToString() + " years , "
+          + month.ToString() + " months , "
+          + day.ToString() + " days.";
+      }
+      //計算time_until_expiration
+      if (!string.IsNullOrEmpty(lbl_certificate_expiry_date.Text))
+      {
+        DateTime dt_expiry = DateTime.Parse(lbl_certificate_expiry_date.Text);
+        int year = dt_expiry.Year - DateTime.Now.Year; //相差的年份
+        int month = dt_expiry.Month - DateTime.Now.Month;  //相差的月份 
+        int day = dt_expiry.Day - DateTime.Now.Day;//相差的天數 
+        lbl_time_until_expiration.Text = year.ToString() + " years , "
+          + month.ToString() + " months , "
+          + day.ToString() + " days.";
+      }
+
+      Label lbl_renewal_confirmation_check = e.Row.FindControl("lbl_renewal_confirmation_check") as Label;
+      switch (lbl_renewal_confirmation_check.Text)
+      {
+        case "1":
+          lbl_renewal_confirmation_check.Text = "Automatic Renewal Service provided by WoWi(WoWi 提供自動更新服務)";
+          break;
+        case "2":
+          lbl_renewal_confirmation_check.Text = "Renewal Confirmed(更新確認)";
+          break;
+        case "3":
+          lbl_renewal_confirmation_check.Text = "No Need(無須更新)";
+          break;
+        default:
+          lbl_renewal_confirmation_check.Text = "Not Setup(未設定)";
+          break;
+      }
+    }
+  }
+  public override void VerifyRenderingInServerForm(Control control)
+  {
+
+  }
+
+  protected void SqlDataSourceTarget_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+  {
+    //if (Cache["IsMailCertificateRenewalAlert"] != null)
+    if (IsAlertMailExist())
+    {
+      e.Cancel = true;
+    }
+  }
+
+  protected bool IsAlertMailExist()
+  {
+    int counter = 0;
+    using (SqlConnection cn =
+      new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["WoWiConnectionString"].ConnectionString))
+    {
+      cn.Open();
+      SqlCommand cmd = new SqlCommand("Select count(*) from AlertMail Where alertmail_date = @alertmail_date", cn);
+      cmd.Parameters.AddWithValue("@alertmail_date", DateTime.Now.ToShortDateString());
+      counter = (int)cmd.ExecuteScalar();
     }
 
-    protected void GridViewProjectTarget_RowDataBound(object sender, GridViewRowEventArgs e)
+    if (counter == 0)
     {
-        if (e.Row.RowType == DataControlRowType.DataRow)
-        {
-            Label lbl_certificate_issue_date = e.Row.FindControl("lbl_certificate_issue_date") as Label;
-            Label lbl_certificate_expiry_date = e.Row.FindControl("lbl_certificate_expiry_date") as Label;
-
-            Label lbl_certificate_duration = e.Row.FindControl("lbl_certificate_duration") as Label;
-            Label lbl_time_until_expiration = e.Row.FindControl("lbl_time_until_expiration") as Label;
-            Label lbl_today_date = e.Row.FindControl("lbl_today_date") as Label;
-            lbl_today_date.Text = DateTime.Now.ToString("yyyy/MM/dd");
-            //計算certificate_duration
-            if (!string.IsNullOrEmpty(lbl_certificate_issue_date.Text) && !string.IsNullOrEmpty(lbl_certificate_expiry_date.Text))
-            {
-                DateTime dt_issue = DateTime.Parse(lbl_certificate_issue_date.Text);
-                DateTime dt_expiry = DateTime.Parse(lbl_certificate_expiry_date.Text);
-                int year = dt_expiry.Year - dt_issue.Year; //相差的年份
-                int month = dt_expiry.Month - dt_issue.Month;  //相差的月份 
-                int day = dt_expiry.Day - dt_issue.Day;//相差的天數 
-                lbl_certificate_duration.Text = year.ToString() + " years , "
-                  + month.ToString() + " months , "
-                  + day.ToString() + " days.";
-            }
-            //計算time_until_expiration
-            if (!string.IsNullOrEmpty(lbl_certificate_expiry_date.Text))
-            {
-                DateTime dt_expiry = DateTime.Parse(lbl_certificate_expiry_date.Text);
-                int year = dt_expiry.Year - DateTime.Now.Year; //相差的年份
-                int month = dt_expiry.Month - DateTime.Now.Month;  //相差的月份 
-                int day = dt_expiry.Day - DateTime.Now.Day;//相差的天數 
-                lbl_time_until_expiration.Text = year.ToString() + " years , "
-                  + month.ToString() + " months , "
-                  + day.ToString() + " days.";
-            }
-
-            Label lbl_renewal_confirmation_check = e.Row.FindControl("lbl_renewal_confirmation_check") as Label;
-            switch (lbl_renewal_confirmation_check.Text)
-            {
-                case "1":
-                    lbl_renewal_confirmation_check.Text = "Automatic Renewal Service provided by WoWi(WoWi 提供自動更新服務)";
-                    break;
-                case "2":
-                    lbl_renewal_confirmation_check.Text = "Renewal Confirmed(更新確認)";
-                    break;
-                case "3":
-                    lbl_renewal_confirmation_check.Text = "No Need(無須更新)";
-                    break;
-                default:
-                    lbl_renewal_confirmation_check.Text = "Not Setup(未設定)";
-                    break;
-            }
-        }
+      return false;
     }
-    public override void VerifyRenderingInServerForm(Control control)
+    else
+    {
+      return true;
+    }
+  }
+
+  protected void SetAlertMail()
+  {
+    try
+    {
+      using (SqlConnection cn =
+       new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["WoWiConnectionString"].ConnectionString))
+      {
+        cn.Open();
+        SqlCommand cmd =
+          new SqlCommand("Insert into AlertMail(alertmail_date,IsMailCertificateRenewalAlert) values(@alertmail_date,@IsMailCertificateRenewalAlert)", cn);
+        cmd.Parameters.AddWithValue("@alertmail_date", DateTime.Now.ToShortDateString());
+        cmd.Parameters.AddWithValue("@IsMailCertificateRenewalAlert", true);
+        cmd.ExecuteNonQuery();
+      }
+    }
+    catch (Exception)
     {
 
     }
+  }
 
-    protected void SqlDataSourceTarget_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
-    {
-        if (Cache["IsMailCertificateRenewalAlert"] != null)
-        {
-            e.Cancel = true;
-        }
-    }
 }
